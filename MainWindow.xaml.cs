de0e05c2d39e68bf377fe053ac1bd44ded6f98f3
Hai Pham <ngochai285nd@gmail.com>
2023-05-22 19:52:55 +0700

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,8 +23,14 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         private string[] NumberKeyPad = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private string[] operators = { "+", "−", "✕", "÷" };
+        private string operand1 = "";
+        private string? math = "";
+        private string operand2 = "";
+
         public MainWindow()
         {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             InitializeComponent();
         }
 
@@ -32,16 +40,36 @@ namespace Calculator
             string? text = button.Content.ToString();
             if (NumberKeyPad.Contains(text))
             {
-                if (Result.Text == "0")
+                if (Result.Text == "0" || Result.Text == "Error")
                 {
                     Result.Text = text;
+                    EnableFunctionButtons(true);
                 }
                 else if (Result.Text.Length < 10)
                 {
                     Result.Text += text;
                 }
             }
+            else if (operators.Contains(text))
+            {
+                
+            }
+            else if (button == btnDot)
+            {
+                if (!Result.Text.Contains("."))
+                {
+                    Result.Text += ".";
+                }
+            }
             else if (button == btnC)
+            {
+                Result.Text = "0";
+                math = "";
+                operand1 = "0";
+                operand2 = "0";
+                EnableFunctionButtons(true);
+            }
+            else if (button == btnCE)
             {
                 Result.Text = "0";
             }
@@ -70,6 +98,53 @@ namespace Calculator
                     }
                 }
             }
+            else if (button == btnCal)
+            {
+                if (math != "")
+                {
+                    Result.Text = Calculate(operand1, operand2, math);
+                    math = "";
+                }
+            }
+        }
+
+        private string Calculate(string operand1, string operand2, string? math)
+        {
+            operand2 = Result.Text;
+            double a = Convert.ToDouble(operand1);
+            double b = Convert.ToDouble(operand2);
+            string result = "";
+            switch (math)
+            {
+                case "+": result = Convert.ToString(a + b); break;
+                case "−": result = Convert.ToString(a - b); break;
+                case "✕": result = Convert.ToString(a * b); break;
+                case "÷":
+                    if ((int)b == b && b == 0)
+                    {
+                        result = "Error";
+                        EnableFunctionButtons(false);
+                    }
+                    else
+                    {
+                        result = Convert.ToString(a / b);
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        private void EnableFunctionButtons(bool status)
+        {
+            btnDivide.IsEnabled = status;
+            btnMultiple.IsEnabled = status;
+            btnPlusMinus.IsEnabled = status;
+            btnCal.IsEnabled = status;
+            btnMinus.IsEnabled = status;
+            btnAdd.IsEnabled = status;
+            btnDot.IsEnabled = status;
+            btnCE.IsEnabled = status;
+            btnBackspace.IsEnabled = status;
         }
     }
 }
